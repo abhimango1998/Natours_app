dc77bfe8e173d985d52699e3882b574a411ec609
abhimango1998 <abhishek.kumar@mangoitsolutions.in>
2025-08-29 17:51:43 +0530

    create reusable APIFeatures to handle filters, pagination, sorting etc

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.getTop5CheapestTours = (req, res, next) => {
   req.query.limit = 5;
@@ -8,47 +9,16 @@ exports.getTop5CheapestTours = (req, res, next) => {
 };
 
 exports.getTours = async (req, res) => {
-  // BUILD QUERY
-  // 1A) Filtering
-  const queryObj = { ...req.query };
-  const excludedFields = ["page", "limit", "sort", "fields"];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  // 1B) Advanced Filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-  let query = Tour.find(JSON.parse(queryStr));
-
-  // 2) Sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-
-  // 3) Fields limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.split(",").join(" ");
-    query = query.select(fields);
-  } else {
-    query = query.select("-__v");
-  }
-
-  // 3) Pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 10;
-  const skip = (page - 1) * limit;
-
-  query = query.skip(skip).limit(limit);
   try {
-    if (req.query.page) {
-      const totalTours = await Tour.countDocuments();
-      if (skip >= totalTours) throw new Error("This page doesn't exists");
-    }
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    const tours = await query;
+    const tours = await features.query;
+
     res.status(200).json({
       status: "success",
       results: tours.length,
@@ -82,28 +52,6 @@ exports.createTour = async (req, res) => {
     });
   }
 };
-
-// exports.createTour = async (req, res) => {
-//   const { name, price, rating } = req.body;
-
-// const newTour = new Tour({
-//   name,
-//   rating,
-//   price,
-// });
-
-//   try {
-// const tour = await newTour.save();
-//     return res.status(201).json({
-//       status: "success",
-//       data: {
-//         tour: tour,
-//       },
-//     });
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// };
 
 exports.getTour = async (req, res) => {
   const { id } = req.params;
